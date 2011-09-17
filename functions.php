e6a32abfa3122f9a6a9816ab264533b1280b3382
Vlad <putyn@ymail.com>
2011-09-17 11:41:49 +0300

    17/09/2011 update

--- functions.php
+++ functions.php
@@ -9,7 +9,6 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 */
-r
 function afraid_login() {
  GLOBAL $foo;
 
